fe0e301694704c25ea4933fdd7ef90eb91aa5e7c
unknown <justbuchanan@gmail.com>
2011-01-28 14:55:37 -0500

    added Arm class

---
+++ FRC2011/Arm.h
@@ -0,0 +1,43 @@
+
+#ifndef ARM_H
+#define ARM_H
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+class Arm {
+	
+	
+	float shoulderAngle();	//	0 is horizontal.  vertical is pi / 2
+	float elbowAngle();		//	0 is fully extended.  
+	
+	
+};
+
+
+
+
+
+
+#endif	//	ARM_H
