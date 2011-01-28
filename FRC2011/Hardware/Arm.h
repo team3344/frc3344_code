d90d4b28fd3b4ff909a7c115ba15e57f9726a879
unknown <justbuchanan@gmail.com>
2011-01-28 15:19:48 -0500

    worked on arm

--- FRC2011/Hardware/Arm.h
+++ FRC2011/Hardware/Arm.h
@@ -28,7 +28,7 @@ counter-clockwise is positive
 
 class Arm {
 	
-	
+	//	arm position
 	float shoulderAngle();	//	0 is horizontal.  vertical is pi / 2
 	float elbowAngle();		//	0 is fully extended.  
 	
@@ -39,11 +39,18 @@ class Arm {
 	void lowerShoulder();
 	
 	//	elbow adjustment
-	
+	void holdElbow();
+	void raiseElbow();
+	void lowerElbow();
 	
 	
 private:
+	//	solenoid that controls shoulder cylinder
 	ThreeStateSolenoid *_shoulderSolenoid;
+	
+	//	elbow
+	Jaguar *_shoulderDensoLeft;
+	Jaguar *_shoulderDensoRight;
 	
 };
 
