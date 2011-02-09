cd00aa59a42733c4391f76ba8a3e40e78553ce7d
Justin Buchanan <justbuchanan@gmail.com>
2011-02-09 13:02:09 -0500

    began SafeDrive & Vector classes

---
+++ FRC2011/Hardware/SafeDrive.h
@@ -0,0 +1,29 @@
+
+#ifndef SAFEDRIVE_H_
+#define SAFEDRIVE_H_
+
+#include "wpilib.h"
+
+
+
+
+class SafeDrive : public RobotDrive {
+
+
+
+
+
+
+
+
+	double maxAcceleration;
+
+
+
+
+};
+
+
+
+
+#endif /* SAFEDRIVE_H_ */
---
+++ FRC2011/Hardware/Vector.h
@@ -0,0 +1,43 @@
+
+
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+
+
+
+
+
+
+
+class Vector {
+
+	Vector(double x, double y, double z = 0);
+	Vector(double magnitude, double direction);
+	~Vector();
+
+
+
+	//	FIXME: dot product, cross product, add, subtract, scalar multiply
+
+
+	double x, y, z;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* VECTOR_H_ */
