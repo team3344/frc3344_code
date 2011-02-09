4adc4e55ddb8fee913ea5f87439b2cad8a219832
unknown <justbuchanan@gmail.com>
2011-02-09 15:08:57 -0500

    implemented Vector class

--- FRC2011/Hardware/Vector.cpp
+++ FRC2011/Hardware/Vector.cpp
@@ -1,1 +1,52 @@
 
+#include "wpilib.h"
+
+
+Vector::Vector(double x, double y, double z = 0)
+{
+	this.x = x;
+	this.y = y;
+	this.z = z;
+}
+
+Vector::Vector(double magnitude, double angle)
+{
+	this.x = magnitude * cos(angle);
+	this.y = magnitude * sin(angle);
+}
+
+
+//	vector operations
+
+Vector Vector::operator*(double scalar)
+{
+	double new_x = scalar * this.x;
+	double new_y = scalar * this.y;
+	double new_z = scalar * this.z;
+	
+	return Vector(new_x, new_y, new_z);
+}
+
+double Vector::operator*(const Vector &other)	//	dot product
+{
+	return (this.x * other.x) + (this.y * other.y) + (this.z * other.z);
+}
+
+Vector Vector::operator+(const Vector &other)
+{
+	double new_x = this.x + other.x;
+	double new_y = this.y + other.y;
+	double new_z = this.z + other.z;
+	
+	return Vector(new_x, new_y, new_z);
+}
+
+Vector Vector::operator-(const Vector &other)
+{
+	double new_x = this.x - other.x;
+	double new_y = this.y - other.y;
+	double new_z = this.z - other.z;
+	
+	return Vector(new_x, new_y, new_z);
+}
+
--- FRC2011/Hardware/Vector.h
+++ FRC2011/Hardware/Vector.h
@@ -5,39 +5,22 @@
 
 
 
-
-
-
-
-
 class Vector {
 
 	Vector(double x, double y, double z = 0);
-	Vector(double magnitude, double direction);
-	~Vector();
+	Vector(double magnitude, double angle);
 
-
-
-	//	FIXME: dot product, cross product, add, subtract, scalar multiply
-
-
+	
+	Vector operator*(double scalar);	//	scalar multiplication
+	double operator*(const Vector &other);	//	dot product
+	
+	Vector operator+(const Vector &other);	//	vector addition & subtraction
+	Vector operator-(const Vector &other);	//
+	
+	
+	//	components
 	double x, y, z;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #endif /* VECTOR_H_ */
