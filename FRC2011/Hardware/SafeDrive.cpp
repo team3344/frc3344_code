dc9c0277b63f2c06db29034dd5f981eab5fa2896
unknown <justbuchanan@gmail.com>
2011-02-10 15:18:06 -0500

    worked on SafeDrive

--- FRC2011/Hardware/SafeDrive.cpp
+++ FRC2011/Hardware/SafeDrive.cpp
@@ -2,36 +2,63 @@
 #include "SafeDrive.h"
 
 
-double SafeDrive::maxAllowedAcceleration(double direction)
+SafeDrive::SafeDrive(double wheelbaseWidth, double wheelbaseLength, FUNCPTR cgHeightFunction)
+{
+	_wheelbaseWidth = wheelbaseWidth;
+	_wheelbaseLength = wheelbaseLength;
+	_cgHeightFunction = cgHeightFunction;
+	
+	//	FIXME: initialize??????
+}
+
+SafeDrive::~SafeDrive()
 {
 	//	FIXME: implement
 }
 
+
+void SafeDrive::setSafeModeActive(bool active)
+{
+	_safeModeActive = active;
+	
+	//	FIXME: activate protection task???????
+}
+
+bool SafeDrive::safeModeActive()
+{
+	return _safeModeActive;
+}
+
+
+
+/********************	Calculations	********************/
+
+double SafeDrive::maxAllowedAcceleration(double direction)
+{
+	//	FIXME: implement
+	
+}
+
+
+double SafeDrive::calculateVelocity(double left, double right)
+{
+	
+}
 
 
 //	given the left & right wheel speeds, finds the turn radius
 double SafeDrive::calculateTurnRadius(double left, double right)
 {
-	//	FIXME: implement
-
-
 	//	FIXME: does this work for all cases?????  which doesn't it work for???
 	double radius = (wheelBaseWidth * (left + right)) / (2 * (left - right));
 
-
-
-
-
-
 	return radius;
 }
-
 
 double SafeDrive::calculateCentripetalAcceleration(double velocity, double turnRadius)
 {
 	return abs( pow(velocity, 2) / turnRadius );	//	v^2 / r
 }
-
 
 
 
--- FRC2011/Hardware/SafeDrive.h
+++ FRC2011/Hardware/SafeDrive.h
@@ -6,35 +6,53 @@
 
 
 
-
 class SafeDrive : public RobotDrive {
 
+	SafeDrive(double wheelbaseWidth, double wheelbaseLength, FUNCPTR cgHeightFunction = NULL);
+	~SafeDrive();
 
-
-	double wheelbaseWidth, wheelbaseLength;
-
-
+	
+	
+	void Drive(double left, double right);
+	
+	//	FIXME: add TankDrive & ArcadeDrive functions!
+	
+	
+	
+	
+	
+	void setSafeModeActive(bool active);
+	bool safeModeActive();
+	
+	
+	
+	/**********		Calculations	**********/
+	
 	//	direction: 0 is right, PI / 2 is straight ahead, etc.
 	double maxAllowedAcceleration(double direction);
-
-
 
 
 	//	given the left & right wheel speeds, finds the turn radius
 	//	negative values = right turn, positive = left turn
 	double calculateTurnRadius(double left, double right);
 
+	double calculateVelocity(double left, double right);
 
 	double calculateCentripetalAcceleration(double velocity, double turnRadius);
-
-	double momentOfInertia;
-	double robotRadius;
-	double cgHeight;
-	double maxAcceleration;
-
+	
+	
+private:
+	
+	bool _safeModeActive;
+	Task _protectionTask;
+	double _wheelbaseWidth, _wheelbaseLength;
+	FUNCPTR _cgHeightFunction;
+	
+	//	max velocity???????
 };
 
 
 
 
 #endif /* SAFEDRIVE_H_ */
+
